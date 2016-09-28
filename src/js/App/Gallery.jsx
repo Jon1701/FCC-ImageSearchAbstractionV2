9235f0e300c009fca8c58d2364c6c1437ec5587a
Jon Balon <development@jonbalon.com>
2016-09-28 17:50:06 -0400

    Added <Gallery/> component

---
+++ src/js/App/Gallery.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Gallery extends React.Component {
+
+  // Component Constructor.
+  constructor(props) {
+    super(props);
+  };
+
+  // Component render.
+  render() {
+    return (
+      <div id="section-gallery" className="boxshadow padding-10">
+        Gallery
+      </div>
+    );
+  };
+
+};
