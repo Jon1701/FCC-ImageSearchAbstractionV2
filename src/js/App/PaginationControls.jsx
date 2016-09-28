b68b6ec337a802723fb17d6c35248a0b5c058fa7
Jon Balon <development@jonbalon.com>
2016-09-28 17:50:36 -0400

    Added <PaginationControls/>

---
+++ src/js/App/PaginationControls.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class PaginationControls extends React.Component {
+
+  // Component Constructor.
+  constructor(props) {
+    super(props);
+  };
+
+  // Component render.
+  render() {
+    return (
+      <div id="section-pagination-controls" className="boxshadow padding-10">
+        Pagination Controls
+      </div>
+    );
+  };
+
+};
