664ce26a55d88cc3b81f52bc3769ceb6a64b263f
Jon Balon <development@jonbalon.com>
2016-09-28 20:42:54 -0400

    Added image search result display

--- src/js/App/Gallery.jsx
+++ src/js/App/Gallery.jsx
@@ -9,9 +9,36 @@ export default class Gallery extends React.Component {
 
   // Component render.
   render() {
+
+    // Will hold the rendered HTML of all the photos returned by the search.
+    var html;
+
+    // Pupulate html variable if a search was done prior.
+    if (this.props.latestResponse) {
+
+      // Images returned by search results.
+      var results = this.props.latestResponse.results;
+
+      // Build an array of HTML structure for each image.
+      html = results.map((image, idx, arr) => {
+
+        // Unique key to suppress React warnings.
+        var key = image.link + '_' + idx;
+
+        return (
+          <div key={key} className="container-search-result col-xs-12 col-sm-6 col-md-4 col-lg-3 boxshadow padding-10">
+            <a href={image.link} target="_blank">
+              <img src={image.link} className="img-responsive thumbnail" alt={image.title}/>
+            </a>
+          </div>
+        );
+      });
+
+    };
+
     return (
-      <div id="section-gallery" className="boxshadow padding-10">
-        Gallery
+      <div id="section-gallery">
+          {html}
       </div>
     );
   };
--- src/js/App/SearchResults.jsx
+++ src/js/App/SearchResults.jsx
@@ -26,7 +26,7 @@ export default class SearchResults extends React.Component {
     return (
       <div id="section-search-results" className={"myClasses"}>
         <PaginationControls latestResponse={this.props.latestResponse} sendRequest={this.props.sendRequest}/>
-        <Gallery/>
+        <Gallery latestResponse={this.props.latestResponse}/>
       </div>
     );
   };
