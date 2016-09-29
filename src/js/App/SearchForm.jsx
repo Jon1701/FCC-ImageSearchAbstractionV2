c0ad926cc59e3abe6acedee73c88bba916a33f95
Jon Balon <development@jonbalon.com>
2016-09-28 21:09:13 -0400

    Added form validation, minor style changes

--- src/js/App/SearchForm.jsx
+++ src/js/App/SearchForm.jsx
@@ -6,8 +6,36 @@ export default class SearchForm extends React.Component {
   constructor(props) {
     super(props);
 
+    // Default state.
+    // Form validation.
+    this.state = {
+      isValid: false
+    };
+
     // Bind methods to component instance.
     this.performSearch = this.performSearch.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+  };
+
+  // Function to determine if the form is valid.
+  validateForm() {
+
+    // Get the search string.
+    var searchString = document.querySelector('#form-search > input[type="text"]').value;
+
+    // By deafault, form is invalid.
+    var isValid = false;
+
+    // Check if the search box is of at least length 3 not including whitespace.
+    if (searchString.trim().length >= 3) {
+      isValid = true;
+    };
+
+    // Update validation flag in state.
+    this.setState({
+      isValid: isValid
+    });
+
   };
 
   // Sends a request to the server to perform an image search.
@@ -31,8 +59,8 @@ export default class SearchForm extends React.Component {
       <div id="section-search-form">
 
         <form id="form-search">
-          <input type="text" placeholder="Enter some keywords" defaultValue="star trek"/>
-          <button type="submit" onClick={this.performSearch}>Go</button>
+          <input type="text" onChange={this.validateForm} placeholder="Enter some keywords" defaultValue="star trek"/>
+          <button type="submit" onClick={this.performSearch} disabled={this.state.isValid == false}>Go</button>
         </form>
 
       </div>
